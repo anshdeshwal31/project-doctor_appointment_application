d394738c729db56c80c359bff4ebb77dcf85a4d5
anshdeshwal31 <anshdeshwal0011@gmail.com>
2024-11-03 02:09:09 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createBrowserRouter , RouterProvider} from "react-router-dom"
+
+const App = () => {
+  return (
+    <div className='bg-purple-500 text-gray-300'>App</div>
+  )
+
+}
+
+export default App;
